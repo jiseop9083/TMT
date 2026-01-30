d90255766729ac0f7445fd9aa1eda8f646dd5056
이진혁 <jinhyeok@jinhyeok-ui-MacBookPro.local>
2026-01-30 16:29:34 +0900

    Fix: Directory Structure

--- bench/single_container_with_profiler/auto_topic_enabled.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
-sh "${DIR}/apply_metrics_server.sh"
-sh "${DIR}/install_monitoring.sh"
-sh "${DIR}/install_strimzi.sh"
-sh "${DIR}/apply_cluster.sh"
-sh "${DIR}/script.sh"
--- bench/single_container_with_profiler/install_monitoring.sh
+++
@@ -1,3 +0,0 @@
-helm install monitoring prometheus-community/kube-prometheus-stack \
-  -n monitoring --create-namespace \
-  --set prometheus-node-exporter.hostRootFsMount.enabled=false
